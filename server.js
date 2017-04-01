a44ecbe2019e77c7865900cb817b093acd5ea082
Sugat Nayak <sugatknayak@Sugats-MacBook-Air.local>
2017-04-01 18:13:20 +0530

    Update localhost

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'http'){
-    next();
+  if (req.headers['x-forwarded-proto'] === 'https'){
+    res.redirect('http://' + req.hostname + req.url);
+
   }
   else{
-    res.redirect('http://' + req.hostname + req.url);
+    next();
   }
 });
 
