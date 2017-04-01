3541809f377d8e54ae79b3da53591438a52b8351
Sugat Nayak <sugatknayak@Sugats-MacBook-Air.local>
2017-04-01 17:27:47 +0530

    Added

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -3,8 +3,10 @@ var React = require('react');
 var Example = React.createClass({
   render: function(){
     return(
-
+      <div>
       <h3>Example Component</h3>
+      <p1>Hello</p1>
+      </div>
     );
 
 
